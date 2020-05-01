489a206989fe6027a3cd2169509772212d7cd984
Ethan Breckenridge <ekb@vultr.guest>
2020-05-01 02:13:02 +0000

    adding forever scripts for server

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# kills the bot and restarts it, restarting it forever
+# if it fails
+
 CURRENT_DIRECTORY="$(dirname $(realpath "$0"))"
 cd "$CURRENT_DIRECTORY"
 
-while true; do
-    date
-    pipenv run python3 bot.py | tee "$CURRENT_DIRECTORY/bot.log"
-done
+forever stopall
+
+forever start --uid "plant-discord-bot" \
+	--logFile "${CURRENT_DIRECTORY}/bot.log" \
+	--append --spinSleepTime 5000  --minUptime 10000 \
+	--sourceDir "$CURRENT_DIRECTORY" \
+	-c "bash" "pipenv_wrapper.sh"
+
